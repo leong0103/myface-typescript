c019a26a23b0452051b2fb884dbd090b2a360a24
Leo Ng <Leo.Ng@softwire.com>
2022-08-25 15:04:29 +0100

    finished mobile nav bar

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Add your JavaScript here.
+// });
+var colors = ["grey", "white"];
+
+    var colorIndex = 0;
+    function changeColor() {
+        var col = document.getElementById("body");
+        var header = document.getElementById("header");
+
+
+        // if( colorIndex >= colors.length ) {
+        //     colorIndex = 0;
+        // }
+        col.classList.toggle("dark-mode");
+        header.classList.toggle("dark-mode");
+
+
+        // col.style.backgroundColor = colors[colorIndex];
+        // colorIndex++;
+    }
+
+    function showNavBar(){
+      // const mobileNav = document.getElementsByClassName("mobile-nav-bar")
+      document.getElementById("mobile-nav-bar").style.display = "flex"
+    }
+
+    function closeNavBar(){
+      // const mobileNav = document.getElementsByClassName("mobile-nav-bar")
+      document.getElementById("mobile-nav-bar").style.display = "none"
+    }
